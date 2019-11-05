4907580a85627759556688cb0e04ed961616feb5
Katie Emerson <kbemerson27@gmail.com>
2019-11-05 10:24:06 -0700

    Day 1

---
+++ app.js
@@ -0,0 +1,12 @@
+// alert("Now I have a file");
+
+document.body.style.background = "red";
+
+setTimeout(() => {
+document.body.style.bakground = "white";
+}, 2000)
+
+setInterval(() => {
+    // document.body.style.bakground = "white";
+    console.log("Hi!")
+}, 2000)
