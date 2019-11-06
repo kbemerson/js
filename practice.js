6e765c8c46592fd475ddc750c0456103aa903618
Katie Emerson <kbemerson27@gmail.com>
2019-11-06 11:00:47 -0700

    Broken

---
+++ practice.js
@@ -0,0 +1,32 @@
+// cs.innerHTML = "Changed";
+
+// cs.append("Now");
+
+// setTimeout(() => {
+//     cs.append("Later");
+// }, 2000);
+
+// let x = 0;
+
+// setInterval(() => {
+//     empty.innerHTML = x + " seconds";
+//     x++;
+//     if (x > 5) {
+//         cs.innerHTML = "Done Counting";
+//     } else {
+//         cs.innerHTML = "Not DOne Counting";
+//     }
+// } 1000);
+
+// let say = function(word) {
+//     cs.append("Saying" + word;)
+// };
+
+// say("hi");
+
+let list = [1, 2, 3];
+let i = 0
+while (i < list.length) {
+    console.log("Logging" + list[i])
+    i++;
+}
